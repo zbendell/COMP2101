9cd284bea3819b22f510c1416accdf800f15a3c0
Zachary <zbendell@gmail.com>
2020-10-07 12:38:25 -0400

    Lab 1 Files

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -5,8 +5,8 @@
 #This is a silly way of creating the output text by starting with something else and stream editing it in a pipeline
 echo -n "helb wold" |
   sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |
-  tr "h" "H"
-  tr "w" "W"
+  tr "h" "H" |
+  tr "w" "W" |
   awk '{print $1 "\x20" $2 "\41"}'
-bc <<< "(($$ * 4 - 24)/2 + 12)/2"
+bc <<< "(($$ * 4 - 24)/2 + 12)/2" |
   sed 's/^/I am process # /'
